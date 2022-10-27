3ae8de8effc66e163a9533bacfd8939eb7ef1375
BuildTools <unconfigured@null.spigotmc.org>
2022-10-27 08:56:39 -0600

    init commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'oshop-ed3ab',
+    appId: '1:473733197286:web:2010c062a5dd9d36db7a27',
+    databaseURL: 'https://oshop-ed3ab-default-rtdb.firebaseio.com',
+    storageBucket: 'oshop-ed3ab.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBN5n8KgWz4LXFNKgjh7irx9DsIB9W0Yoc',
+    authDomain: 'oshop-ed3ab.firebaseapp.com',
+    messagingSenderId: '473733197286',
+    measurementId: 'G-3RFN6L3M9B',
+  }
 };
 
 /*
